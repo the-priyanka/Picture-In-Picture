3eddd7f39e2c28477eddfcac1b8f33b53bdc3f39
Priyanka <original.manaria@gmail.com>
2021-12-16 13:01:30 +0530

    I am checking functionality

--- app.js
+++ app.js
@@ -1,23 +1,23 @@
-const videoElement = document.getElementById('video')
-const button = document.getElementById('button')
+const videoElement = document.getElementById("video");
+const button = document.getElementById("button");
 
 async function selectMediaStream() {
   try {
-    const mediaStream = await navigator.mediaDevices.getDisplayMedia()
-    videoElement.srcObject = mediaStream
+    const mediaStream =
+      await navigator.mediaDevices.getDisplayMedia();
+    videoElement.srcObject = mediaStream;
     videoElement.onloadedmetadata = () => {
-      videoElement.play()
-    }
+      videoElement.play();
+    };
   } catch (error) {
-    // catch Error Here 
-    console.log(error)
+    // catch Error Here
+    console.log(error);
   }
 }
 
 button.addEventListener("click", async () => {
-  button.disabled = true
-  await videoElement.requestPictureInPicture()
-  button.disabled = false
-
-})
-selectMediaStream()
+  button.disabled = true;
+  await videoElement.requestPictureInPicture();
+  button.disabled = false;
+});
+selectMediaStream();
